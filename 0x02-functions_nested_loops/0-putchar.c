72f579c7081f3905cef1f7ef043539989a2e52c3
Francoise NIYONKESHA <niyofrancoise02@gmail.com>
2023-06-20 07:59:44 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
 
-
-	int main(void)
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 	{
 	_putchar('_');
 	_putchar('p');
